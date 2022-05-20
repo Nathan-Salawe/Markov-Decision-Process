5d6b1eea2779722ad86b38dab9431342d4dff0a5
Nathan-Salawe <salawenath@gmail.com>
2022-05-20 22:30:01 +0100

    Update MDP OOP.py

--- MDP OOP.py
+++ MDP OOP.py
@@ -136,9 +136,6 @@ class LayoutPage(Frame):
         
         Frame.__init__(self, parent)
         
-        self.colormap = ['#e0ffd0', '#e6ffd8', '#c3ffa2', '#94ff5a', '#75ff2a', '#59ff00', 
-                         '#56f400', '#52e900', '#4ddb00', '#45c400']
-        
         self.obstacle_color = 'light grey'
         self.states_color = 'white'
         self.agent_color = 'purple'
@@ -147,15 +144,17 @@ class LayoutPage(Frame):
         self.rowconfigure(0, weight=1)
         self.rowconfigure(1, weight=15)
         self.columnconfigure(0, weight=10)
+        self.policy = False
         
         self.utility_frame = Frame(self)
         self.utility_frame.grid(row=0, column=0, sticky='nsew')
         self.utility_frame.rowconfigure(0, weight=1)
-        self.utility_frame.columnconfigure(0, weight=1)
-        self.utility_frame.columnconfigure(1, weight=1)
-        self.utility_frame.columnconfigure(2, weight=1)
-        self.utility_frame.columnconfigure(3, weight=1)
-        
+        self.utility_frame.columnconfigure(0, weight=3)
+        self.utility_frame.columnconfigure(1, weight=3)
+        self.utility_frame.columnconfigure(2, weight=3)
+        self.utility_frame.columnconfigure(3, weight=3)
+        self.utility_frame.columnconfigure(4, weight=1)
+                
         self.layout_frame = Frame(self)
         self.layout_frame.grid(row=1, column=0, sticky='nsew')
         
@@ -173,37 +172,32 @@ class LayoutPage(Frame):
         self.quit_button.grid(row=0, column=2, sticky='nsew')
         
         self.go_button = Button(self.utility_frame, text='Go!!', fg='green',
-                                command = self.game, borderwidth=0, bg=bg_c, font=small_font)
+                                command = lambda: self.game(self.agent_pos), borderwidth=0, bg=bg_c, font=small_font)
         self.go_button.grid(row=0, column=1, sticky='nsew')
         
-        self.track = []
+        self.status = Label(self.utility_frame, text='', fg='green', 
+                            borderwidth=0, bg=bg_c, font=small_font)
+        self.status.grid(row=0, column=4, sticky='nsew')
         
-        self.mapkey = {}
-        i = 1
-        j = 1
-        for steps in range(len(self.colormap)):
-            self.mapkey[str(i)] = j
-            i = round(i - 0.1, 1)
-            j += 1
             
     def refresh_(self):
-        layout = get_current()
-        self.create_layout(layout, self.layout_frame)
-        self.obstacles(layout)
+        self.layout = get_current()
+        self.create_layout(self.layout, self.layout_frame)
+        self.obstacles(self.layout)
         self.agent_pos = [0, 0]
         self.all_states = {}
         self.available_states = {}
         
-        for i in range(layout[0]):
+        for i in range(self.layout[0]):
             self.update()
-            for j in range(layout[1]):        
+            for j in range(self.layout[1]):        
                 curr = []
                 avai = []
-                if j + 1 < layout[1]:
+                if j + 1 < self.layout[1]:
                     curr.append([i, j+1])
                     if self.lay_grid[i][j+1].cget('bg') == self.states_color:
                         avai.append([i, j+1])
-                if i + 1 < layout[0]:
+                if i + 1 < self.layout[0]:
                     curr.append([i+1, j])
                     if self.lay_grid[i+1][j].cget('bg') == self.states_color:
                         avai.append([i+1, j])
@@ -220,11 +214,13 @@ class LayoutPage(Frame):
         
         self.present_state = self.lay_grid[self.agent_pos[0]][self.agent_pos[1]]
         self.present_state.configure(bg=self.agent_color)
-        self.destination = self.lay_grid[layout[0] - 1][layout[1] - 1]
-        self.destination.configure(bg=self.destination_color)
+        self.destination_coor = [self.layout[0] - 1, self.layout[1] - 1]
+        self.destination = self.lay_grid[self.destination_coor[0]][self.destination_coor[1]]
+        self.destination.configure(bg=self.destination_color, text=1)
         self.destination.configure(borderwidth=5, relief='groove')
         
     def create_layout(self, size, frame):
+        
         self.lay_grid = []
         for row in range(size[0]):
             self.update()
@@ -238,63 +234,88 @@ class LayoutPage(Frame):
             grid_row = []
             for c in range(size[1]):
                 self.update()
-                button = Button(frame, text= ' ', bg=self.states_color, borderwidth=0)
+                button = Button(frame, text= 0, bg=self.states_color, borderwidth=0, 
+                                command = lambda x = [r, c]: self.game(x))
                 button.grid(row=r, column=c, sticky='nsew')
                 grid_row.append(button)
                 self.update()
             self.lay_grid.append(grid_row)
 
-    def change_state(self, current_state):
-        # function for a random search Algorithm that will precede the MDP algorithm
-        p = 0
-        n = 0
-        if self.lay_grid[current_state[0]][current_state[1]].cget('text') == ' ':
-            self.lay_grid[current_state[0]][current_state[1]].configure(text=1)
-            color = self.colormap[0]
-            self.lay_grid[current_state[0]][current_state[1]].configure(bg=color, fg=color)
-            self.update()
-            p = 1
-            n = 1
-        else:
-            prior = self.lay_grid[current_state[0]][current_state[1]].cget('text')
-            txt = round(prior - 0.1, 1)
-            self.lay_grid[current_state[0]][current_state[1]].configure(text=txt)
-            key = self.mapkey[str(txt)]
-            color = self.colormap[key]
-            self.lay_grid[current_state[0]][current_state[1]].configure(bg=color, fg=color)
-            self.update()
-            p = txt
 
-        next_states = self.available_states[tuple(current_state)]
-        states_val = []
-        free_states = []
+    def get_max(self, pos, limit, explore=True):
         
-        for i in next_states:
+        values = {}
+        
+        if pos[0]-1 > -1:
+    
+            v = self.lay_grid[pos[0]-1][pos[1]].cget('text')
+            values[pos[0]-1, pos[1]] = v
+    
+        if pos[0]+1 < limit[0]:
+    
+            v = self.lay_grid[pos[0]+1][pos[1]].cget('text')
+            values[pos[0]+1, pos[1]] = v
+    
+        if pos[1]-1 > -1:
+            v = self.lay_grid[pos[0]][pos[1]-1].cget('text')
+            values[pos[0], pos[1]-1] = v
+    
+        if pos[1]+1 < limit[1]: 
+            v = self.lay_grid[pos[0]][pos[1]+1].cget('text')
+            values[pos[0], pos[1]+1] = v
+        
+        if explore:
+            c = random.choice(list(values.keys()))
+            return [c, values[c]]
+            
+        else:
+            max_v = max(values, key = lambda x: values[x])
+            return [list(max_v), values[max_v]]
+
+        
+    def mdp_algorithm(self):
+        
+        e = 1
+        d = 0.8
+        g = 0.9
+        a = 0.9
+        
+        episode = (self.layout[0] * self.layout[1]) * 12
+        epoch = self.layout[0] * self.layout[1]
+        turns = 0
+        overall = 0
+        cur_pos = self.agent_pos
+        
+        for i in range(episode):
+            turns += 1
+            
+            if turns == 100:
+                self.status.configure(text=turns+overall)
+                turns = 0
+                overall += 100
+            
             self.update()
-            if self.lay_grid[i[0]][i[1]].cget('text') == ' ':
-                free_states.append(i)
-            else:
-                states_val.append([i, self.lay_grid[i[0]][i[1]].cget('text')])
+            for i in range(epoch):
+                self.update()
+                r = random.random()
+                if r < e:
+                    explored = True
+                else:
+                    explored = False
                 
-        self.update()
-        
-        if len(free_states) > 0:    
-            return [random.choice(free_states), [p, n]]
-        else:
-            curr = states_val[0]
-            multi = []
-            for i in states_val:
-                if i[1] > curr[1]:
-                    curr = i
-                    multi = []
-                    multi.append(i)
-                elif i[1] == curr[1]:
-                    multi.append(i)
-            return [random.choice(multi)[0], [p, n]]
-        
-    def MDP_algorithm(self):
-        pass
+                c, v = self.get_max(cur_pos, self.layout, explore=explored)
+                if self.lay_grid[cur_pos[0]][cur_pos[1]].cget('text') == 1 or self.lay_grid[cur_pos[0]][cur_pos[1]].cget('text') == -1:
+                    break
+                else:
+                    v_s = round((1-a)*self.lay_grid[cur_pos[0]][cur_pos[1]].cget('text') + a*(v*g), 3)
+                    self.lay_grid[cur_pos[0]][cur_pos[1]].configure(text= v_s)          
+                cur_pos = c
+            e = e * d
+            cur_pos = [random.randint(0, self.layout[0]-1), 
+                       random.randint(0, self.layout[1]-1)]
+            self.policy = True
 
+            
     def obstacles(self, size):
         # create random obstacles on the layout
         # Needs more work lol
@@ -308,38 +329,45 @@ class LayoutPage(Frame):
             to_edit = random.choices([i for i in range(size[1])], k=no_col_obs)
             for j in to_edit:
                 self.lay_grid[i][j].configure(bg=self.obstacle_color)
+                self.lay_grid[i][j].configure(text=-1)
                 self.update()
 
     def backtrack(self):
-        self.layout_frame.destroy()
+        # self.layout_frame.destroy()
         self.controller.show_frames(StartPage)
         
     def stop_function(self):
         # remember to save th policy after implimenting the mdp
         root.destroy()
     
-    def game(self):
-        points = 0
-        n = 0
-        avg_points = 1
-        avg_unique = 1
+    def game(self, pos):
         
-        i = 1
-        # while avg_points > 0.92 and avg_unique > 0.5:
-        for bb in range(1000):
-            stat = self.change_state(self.agent_pos)
-            next_state = stat[0]
-            points += stat[1][0]
-            n += stat[1][1]
-            self.lay_grid[next_state[0]][next_state[1]].configure(bg=self.agent_color)
-            self.agent_pos = next_state
-            self.update()
-            self.after(10)
-            self.update()
-            avg_points = points/i
-            avg_unique = n/i           
-            i += 1
+            if self.policy == False:
+                self.mdp_algorithm()
+            
+            self.destination.configure(bg=self.destination_color)
+            trail = []
+            self.agent_pos = pos
+            while True:
+
+                next_state, v = self.get_max(self.agent_pos, self.layout, explore=False)
+                
+                self.lay_grid[self.agent_pos[0]][self.agent_pos[1]].configure(bg=self.agent_color)
+                trail.append(self.agent_pos)
+                
+                try:
+                    prev = trail[-2]
+                    self.lay_grid[prev[0]][prev[1]].configure(bg=self.states_color)
+                except IndexError:
+                    pass
+                
+                if self.agent_pos == self.destination_coor:
+                    break
         
+                self.agent_pos = next_state
+                self.update()
+                self.after(100)
+                self.update()
 
 
 app = MDP()
